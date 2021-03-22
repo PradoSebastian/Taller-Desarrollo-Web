d35d91c58d95bdee290a2703ad6a682ddce9f2ff
Juan Sebastián Prado Valero <pradosebas@yahoo.com.co>
2021-03-22 11:54:41 -0500

    Se reubicaron carpetas

--- src/main/java/com/example/demo/services/XmlFormater.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.services;
-
-public class XmlFormater implements Formater {
-
-    public String Format(String message) {
-        return "<element att='bookCard'>" + message + "</element>";
-    }
-}
